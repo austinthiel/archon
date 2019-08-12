a989cd66bad1a28bdd79a4953b9b7dd922c03e76
Austin Thiel <hello@austinthiel.com>
2019-08-12 00:37:55 -0700

    map loads markers via query, w/ map popups

--- packages/archon-react/src/components/home/GoogleMap.jsx
+++ packages/archon-react/src/components/home/GoogleMap.jsx
@@ -1,10 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import { css } from '@emotion/core';
 
-const GoogleMap = props => {
+const GoogleMap = () => {
   const googleMapRef = useRef(null);
 
   let googleMap = null;
+  let infoWindow = null;
 
   useEffect(() => {
     const googleMapScript = document.createElement('script');
@@ -18,6 +19,7 @@ const GoogleMap = props => {
   }, []);
 
   const createGoogleMap = () => {
+    infoWindow = new window.google.maps.InfoWindow();
     googleMap = new window.google.maps.Map(googleMapRef.current, {
       zoom: 11,
       center: {
@@ -28,14 +30,39 @@ const GoogleMap = props => {
   };
 
   const createMarkers = () => {
-    new window.google.maps.Marker({
-      position: { lat: 37.7655478, lng: -122.4569743 },
+    const service = new window.google.maps.places.PlacesService(googleMap);
+
+    const requests = [
+      {
+        query: 'Circus Center',
+        fields: ['name', 'geometry'],
+      },
+      {
+        query: 'Circus Connections',
+        fields: ['name', 'geometry'],
+      },
+    ];
+
+    requests.forEach(request => {
+      service.findPlaceFromQuery(request, (results, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          results.forEach(result => {
+            createMarker(result);
+          });
+        }
+      });
+    });
+  };
+
+  const createMarker = place => {
+    const marker = new window.google.maps.Marker({
       map: googleMap,
+      position: place.geometry.location,
     });
 
-    new window.google.maps.Marker({
-      position: { lat: 37.7509482, lng: -122.4064738 },
-      map: googleMap,
+    window.google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(place.name);
+      infoWindow.open(googleMap, this);
     });
   };
 
@@ -45,7 +72,7 @@ const GoogleMap = props => {
       ref={googleMapRef}
       css={css`
         width: 100%;
-        height: 350px;
+        height: 400px;
       `}
     />
   );
